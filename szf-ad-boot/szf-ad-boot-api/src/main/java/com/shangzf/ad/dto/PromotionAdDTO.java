7d87ac8d881f248e358d3218beed694300301c00
shangzf <784098196@qq.com>
2021-05-16 22:07:19 +0800

    修改json显示日期格式

--- szf-ad-boot/szf-ad-boot-api/src/main/java/com/shangzf/ad/dto/PromotionAdDTO.java
+++ szf-ad-boot/szf-ad-boot-api/src/main/java/com/shangzf/ad/dto/PromotionAdDTO.java
@@ -1,6 +1,8 @@
 package com.shangzf.ad.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -48,11 +50,15 @@ public class PromotionAdDTO implements Serializable {
     /**
      * 开始时间
      */
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date startTime;
 
     /**
      * 结束时间
      */
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date endTime;
 
     /**
