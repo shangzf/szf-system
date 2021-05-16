d2e98b8f3d92b261646ccc3ac0976c16be449fa7
shangzf <784098196@qq.com>
2021-05-16 22:09:20 +0800

    修改请求地址

--- szf-ad-boot/szf-ad-boot-api/src/main/java/com/shangzf/ad/remote/AdRemoteService.java
+++ szf-ad-boot/szf-ad-boot-api/src/main/java/com/shangzf/ad/remote/AdRemoteService.java
@@ -1,6 +1,8 @@
 package com.shangzf.ad.remote;
 
+import com.shangzf.ad.dto.PromotionAdDTO;
 import com.shangzf.ad.dto.PromotionSpaceDTO;
+import com.shangzf.response.ResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "szf-ad-boot")
+@FeignClient(name = "szf-ad-boot",path = "/ad")
 public interface AdRemoteService {
 
     @GetMapping("/space/all")
@@ -20,8 +22,17 @@ public interface AdRemoteService {
     List<PromotionSpaceDTO> getBySpaceKeys(@RequestParam("spaceKeys") String[] spaceKeys);
 
     @PostMapping("/space/saveOrUpdate")
-    void saveOrUpdate(@RequestBody PromotionSpaceDTO dto);
+    ResponseDTO saveOrUpdateSpace(@RequestBody PromotionSpaceDTO dto);
 
     @GetMapping("/space/{id}")
-    PromotionSpaceDTO getById(@PathVariable("id") Long id);
+    PromotionSpaceDTO getSpaceById(@PathVariable("id") Long id);
+
+    @GetMapping("/all")
+    List<PromotionAdDTO> getAllAds();
+
+    @PostMapping("/saveOrUpdate")
+    ResponseDTO saveOrUpdateAd(@RequestBody PromotionAdDTO dto);
+
+    @GetMapping("/{id}")
+    PromotionAdDTO getAdById(@PathVariable("id") Long id);
 }
