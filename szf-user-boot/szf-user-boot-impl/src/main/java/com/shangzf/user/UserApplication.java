9c419dfd10e1e12ab8060116137a238b257ed0da
shangzf <784098196@qq.com>
2021-05-17 09:27:50 +0800

    添加启动类

---
+++ szf-user-boot/szf-user-boot-impl/src/main/java/com/shangzf/user/UserApplication.java
@@ -0,0 +1,15 @@
+package com.shangzf.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@MapperScan("com.shangzf.user.mapper")
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+}
