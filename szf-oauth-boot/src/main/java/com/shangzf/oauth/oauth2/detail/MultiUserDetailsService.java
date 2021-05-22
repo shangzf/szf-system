0faee6433b45575e2977e9ac725883f7f6afd0bb
shangzf <784098196@qq.com>
2021-05-22 21:41:51 +0800

    获取权限

--- szf-oauth-boot/src/main/java/com/shangzf/oauth/oauth2/detail/MultiUserDetailsService.java
+++ szf-oauth-boot/src/main/java/com/shangzf/oauth/oauth2/detail/MultiUserDetailsService.java
@@ -1,12 +1,15 @@
 package com.shangzf.oauth.oauth2.detail;
 
 import com.google.common.collect.Sets;
+import com.shangzf.authority.api.dto.RoleDTO;
+import com.shangzf.authority.api.remote.IRoleRemoteService;
 import com.shangzf.oauth.entity.UserJwt;
 import com.shangzf.oauth.multi.MultiAuthentication;
 import com.shangzf.oauth.multi.MultiAuthenticationContext;
 import com.shangzf.oauth.multi.authenticator.MultiAuthenticator;
 import com.shangzf.user.api.dto.UserDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,17 +20,19 @@ import org.springframework.stereotype.Service;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service("userDetailService")
 public class MultiUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private IRoleRemoteService roleRemoteService;
     private final List<MultiAuthenticator> authenticators;
 
     public MultiUserDetailsService(List<MultiAuthenticator> authenticators) {
         this.authenticators = authenticators;
     }
-
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -53,9 +58,15 @@ public class MultiUserDetailsService implements UserDetailsService {
     /**
      * 获得登录者所有角色的权限集合.
      */
-    private Collection<? extends GrantedAuthority> obtainGrantedAuthorities(UserDTO userDTO) {
-        // TODO 获取权限
-        return Sets.newHashSet(new SimpleGrantedAuthority("NONE"));
+    private Collection<? extends GrantedAuthority> obtainGrantedAuthorities(UserDTO dto) {
+        try {
+            List<RoleDTO> roles = roleRemoteService.getRolesByUserId(dto.getId());
+            log.info("user:{},roles:{}", dto.getName(), roles);
+            return roles.stream().map(role -> new SimpleGrantedAuthority(role.getCode())).collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Sets.newHashSet(new SimpleGrantedAuthority("NONE"));
+        }
     }
 
     private UserDTO authenticate(MultiAuthentication authentication) {
