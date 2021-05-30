d41d633e9a70b3caceeb128291ba773caf3215bf
shangzf <784098196@qq.com>
2021-05-30 21:20:59 +0800

    修改结构

--- szf-authority-boot/szf-authority-boot-impl/src/main/java/com/shangzf/authority/remote/ResourceRemoteService.java
+++ szf-authority-boot/szf-authority-boot-impl/src/main/java/com/shangzf/authority/remote/ResourceRemoteService.java
@@ -12,10 +12,10 @@ import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
