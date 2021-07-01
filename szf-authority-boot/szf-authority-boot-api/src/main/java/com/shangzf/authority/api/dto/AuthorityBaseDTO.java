1fbed286aad487dce37597db1d76f153a7bf4d61
shangzf <784098196@qq.com>
2021-07-01 22:01:24 +0800

    修改结构

--- szf-authority-boot/szf-authority-boot-api/src/main/java/com/shangzf/authority/api/dto/AuthorityBaseDTO.java
+++ szf-authority-boot/szf-authority-boot-api/src/main/java/com/shangzf/authority/api/dto/AuthorityBaseDTO.java
@@ -1,26 +1,15 @@
 package com.shangzf.authority.api.dto;
 
+import com.shangzf.common.web.pojo.dto.BaseDTO;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import java.io.Serializable;
-import java.util.Date;
-
 @Data
-@EqualsAndHashCode(of = "id")
-public class AuthorityBaseDTO implements Serializable {
+@EqualsAndHashCode(callSuper = true)
+public class AuthorityBaseDTO extends BaseDTO {
 
     private static final long serialVersionUID = 5305252239737887218L;
-
-    @Schema(description = "ID")
-    private Long id;
-
-    @Schema(description = "创建时间")
-    private Date createTime;
-
-    @Schema(description = "最后修改时间")
-    private Date lastModifyTime;
 
     @Schema(description = "创建者")
     private String createBy;
@@ -28,3 +17,4 @@ public class AuthorityBaseDTO implements Serializable {
     @Schema(description = "最后修改者")
     private String lasModifyBy;
 }
+
