82c2d0d9aa2cc66cc34e6b551cf9a5cbb6f60bc7
shangzf <784098196@qq.com>
2021-07-01 09:39:21 +0800

    message模块

--- szf-message-boot/szf-message-boot-impl/src/main/java/com/shangzf/message/controller/MessageController.java
+++ szf-message-boot/szf-message-boot-impl/src/main/java/com/shangzf/message/controller/MessageController.java
@@ -1,17 +1,47 @@
 package com.shangzf.message.controller;
 
-
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.shangzf.common.util.ConvertUtil;
+import com.shangzf.common.web.pojo.vo.page.DataGrid;
+import com.shangzf.message.api.dto.MessageDTO;
+import com.shangzf.message.api.dto.param.MessageParam;
+import com.shangzf.message.entity.Message;
+import com.shangzf.message.service.IMessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * <p>
- * 消息 前端控制器
- * </p>
- */
+import java.util.List;
+
 @RestController
-@RequestMapping("/message/message")
+@RequestMapping("/message")
 public class MessageController {
 
+    @Autowired
+    private IMessageService messageService;
+
+    @GetMapping("/page")
+    public DataGrid<MessageDTO> getPage(MessageParam messageParam) {
+        Page<Message> messagePage = messageService.getPage(messageParam);
+        List<MessageDTO> dtoList = ConvertUtil.convertList(messagePage.getRecords(), MessageDTO.class);
+        return new DataGrid<>(dtoList, messagePage.getTotal(), messagePage.getSize(), messagePage.getCurrent());
+    }
+
+    @PutMapping("/read")
+    public Boolean read(@RequestBody Long id) {
+        return messageService.read(id);
+    }
+
+    @PutMapping("/read/add")
+    public Boolean readAll() {
+        return messageService.readAll();
+    }
+
+    @GetMapping("/unread")
+    public Integer unreadCount() {
+        return messageService.unreadCount();
+    }
 }
