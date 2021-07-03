59c6a83c1e150e3424e98866868bcb8b5dabba80
shangzf <784098196@qq.com>
2021-07-04 00:37:15 +0800

    修改结构

--- szf-authority-boot/szf-authority-boot-impl/src/main/java/com/shangzf/authority/config/OpenApiConfig.java
+++ szf-authority-boot/szf-authority-boot-impl/src/main/java/com/shangzf/authority/config/OpenApiConfig.java
@@ -1,7 +1,9 @@
 package com.shangzf.authority.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @OpenAPIDefinition(info = @Info(title = "自研系统", description = "权限接口文档", version = "v0.0.1"))
 //@SecurityScheme(name = "Authorization", type = SecuritySchemeType.HTTP, scheme = "bearer", description = "输入access_token的值", bearerFormat = "JWT")
